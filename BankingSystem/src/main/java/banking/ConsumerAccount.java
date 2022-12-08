576cabadaae7fc6ab58dacc9ec630b21a4d43800
Harsh_Dhoke <73290073+harshdhoke@users.noreply.github.com>
2022-12-08 11:38:39 +0530

    Update ConsumerAccount.java

--- BankingSystem/src/main/java/banking/ConsumerAccount.java
+++ BankingSystem/src/main/java/banking/ConsumerAccount.java
@@ -1,7 +1,28 @@
 package banking;
 
-public class ConsumerAccount  {
+public class ConsumerAccount extends Account{
+	private Person person;
+	private Long accountNumber;
+	private double currentBalance;
+	
 	public ConsumerAccount(Person person, Long accountNumber, int pin, double currentBalance) {
-		// complete the function
+		super(person, accountNumber, pin, currentBalance);
+		this.person = person;
+		this.accountNumber = accountNumber;
+		this.currentBalance = currentBalance;
 	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+
+
+	public double getCurrentBalance() {
+		return currentBalance;
+	}
+	
 }
