c5e3b41b239b5b46add89b79667de7e95f18ef52
Harsh_Dhoke <73290073+harshdhoke@users.noreply.github.com>
2022-12-08 11:34:16 +0530

    Update Bank.java

--- BankingSystem/src/main/java/banking/Bank.java
+++ BankingSystem/src/main/java/banking/Bank.java
@@ -7,43 +7,50 @@ import java.util.LinkedHashMap;
  * {@link #accounts}: List&lt;Long, Account&gt;
  */
 public class Bank implements BankInterface {
-	private LinkedHashMap<Long, Account> accounts;
+	private LinkedHashMap<Long, Account> accounts = new LinkedHashMap<Long, Account>();
 
+	/*Added new comment for testing github*/
 	public Bank() {
-		// complete the function
+		super();
 	}
 
 	private Account getAccount(Long accountNumber) {
-		// complete the function
-        return null;
+		System.out.println("Im here with "+accountNumber);
+        return accounts.get(accountNumber)!=null ? accounts.get(accountNumber): null;
 	}
 
 	public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
+		long newAccountNumber = openNewAccountNumber();
+		accounts.put(newAccountNumber, new CommercialAccount(company, newAccountNumber, pin, startingDeposit));
+		return newAccountNumber;
 	}
 
 	public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
+		long newAccountNumber = openNewAccountNumber();
+		accounts.put(newAccountNumber, new ConsumerAccount(person, newAccountNumber , pin, startingDeposit));
+		return newAccountNumber;
 	}
 
 	public boolean authenticateUser(Long accountNumber, int pin) {
-		// complete the function
-        return true;
+        return getAccount(accountNumber).validatePin(pin);
 	}
 
 	public double getBalance(Long accountNumber) {
-		// complete the function
-        return -1;
+        return getAccount(accountNumber).getBalance();
 	}
 
 	public void credit(Long accountNumber, double amount) {
-		// complete the function
+		getAccount(accountNumber).creditAccount(amount);
 	}
 
 	public boolean debit(Long accountNumber, double amount) {
-		// complete the function
-        return true;
+		return getAccount(accountNumber).debitAccount(amount);
+	}
+	
+	public long openNewAccountNumber()
+	{
+		int lastAccNo = accounts.size();
+		
+		return lastAccNo + 1;
 	}
 }
