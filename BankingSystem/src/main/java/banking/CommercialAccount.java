4e6052015f4a4e9f82f08c47d52d7f13bb9e7070
Harsh_Dhoke <73290073+harshdhoke@users.noreply.github.com>
2022-12-08 11:37:06 +0530

    Update CommercialAccount.java

--- BankingSystem/src/main/java/banking/CommercialAccount.java
+++ BankingSystem/src/main/java/banking/CommercialAccount.java
@@ -1,6 +1,5 @@
 package banking;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,18 +8,19 @@ import java.util.List;
  * Private Variables:<br>
  * {@link #authorizedUsers}: List&lt;Person&gt;<br>
  */
-public class CommercialAccount  {
+public class CommercialAccount extends Account {
 	private List<Person> authorizedUsers;
 
 	public CommercialAccount(Company company, Long accountNumber, int pin, double startingDeposit) {
-		// complete the function
+		super(company, accountNumber,pin, startingDeposit );
 	}
 
+	
 	/**
 	 * @param person The authorized user to add to the account.
 	 */
 	protected void addAuthorizedUser(Person person) {
-		// complete the function
+		authorizedUsers.add(person);
 	}
 
 	/**
@@ -28,7 +28,6 @@ public class CommercialAccount  {
 	 * @return true if person matches an authorized user in {@link #authorizedUsers}; otherwise, false.
 	 */
 	public boolean isAuthorizedUser(Person person) {
-		// complete the function
-        return true;
+        return authorizedUsers.contains(person);
 	}
 }
