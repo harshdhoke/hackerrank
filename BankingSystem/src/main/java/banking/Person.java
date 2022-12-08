c58e59cccb7e0bc3d513c9bc9475f3b96e169578
Harsh_Dhoke <73290073+harshdhoke@users.noreply.github.com>
2022-12-08 11:39:18 +0530

    Update Person.java

--- BankingSystem/src/main/java/banking/Person.java
+++ BankingSystem/src/main/java/banking/Person.java
@@ -1,20 +1,27 @@
 package banking;
 
-public class Person {
+public class Person extends AccountHolder {
 	private String firstName;
 	private String lastName;
+	private int idNumber;
 
 	public Person(String firstName, String lastName, int idNumber) {
-		// complete the function
+		super(idNumber);
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = idNumber;
 	}
 
 	public String getFirstName() {
-		// complete the function
-        return null;
+        return this.firstName;
 	}
 
 	public String getLastName() {
-		// complete the function
-        return null;
+        return this.lastName;
+	}
+	
+	public int getIdNumber()
+	{
+		return this.idNumber;
 	}
 }
